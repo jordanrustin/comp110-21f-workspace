3bad5b946b21ee47c10714eb14660b2e20ac7aa4
Jordan Rustin <jrustin@live.unc.edu>
2021-11-01 23:29:52 -0400

    progress thru oct

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,22 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730308364"
 
 # Define your functions below
+
+
+def invert(one: dict[str, str]) -> dict[str, str]:
+    """Inverts the keys and values of dictionary."""
+    output: dict[str, str] = {}
+    for key in one:
+        output[one[key]] = key
+    return output
+
+
+def favorite_color(two: dict[str, str]) -> str:
+    """Returns the color that appears most frequently."""
+    color: dict[str, int] = {}
+    for key in two:
+        favs = two.get(key)
+        if favs:
+            
