e8012e911cc090120955b8323017cfb5758c3e06
Jordan Rustin <jrustin@live.unc.edu>
2021-11-10 23:46:34 -0500

    progress on ex06

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -10,13 +10,37 @@ def invert(one: dict[str, str]) -> dict[str, str]:
     output: dict[str, str] = {}
     for key in one:
         output[one[key]] = key
+        if key in output:
+            raise KeyError
     return output
 
 
-def favorite_color(two: dict[str, str]) -> str:
+def favorite_color(one: dict[str, str]) -> str:
     """Returns the color that appears most frequently."""
-    color: dict[str, int] = {}
-    for key in two:
-        favs = two.get(key)
-        if favs:
-            
+    color_times: dict[str, int] = {}
+    for key in one:
+        if one[key] in color_times:
+            color_times[one[key]] += 1
+        else:
+            color_times[one[key]] = 1
+        
+    high: int = 0
+    fav: str = ""
+
+    for key in color_times:
+        if color_times[key] > high:
+            high = color_times[key]
+            fav = key
+    return fav
+
+
+def count(freq: list[str]) -> dict[str, int]:
+    """Produces a dictionary where each key is a unique value in the given list and each value associated is the count of the number of times that value appeared in the input list."""
+    result: dict[str, int] = {}
+    
+    for column in freq:
+        if column in result:
+            result[column] += 1
+        else:
+            result[column] = 1
+    return result
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,6 @@
 """Unit tests for dictionary functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+__author__ = "730308364"
